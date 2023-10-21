e0d7dea1a306a7ac6fc20d3f2cde7b0ab34535b0
Geraldo Quende <43283373+gquende@users.noreply.github.com>
2023-10-21 20:00:51 +0100

    FEAT: creating validators

--- backend/src/controllers/authcontroller.js
+++ backend/src/controllers/authcontroller.js
@@ -1,50 +1,44 @@
 import { UserModel } from "../models/user";
 
 export class AuthController {
+  login(email, password) {}
 
+  signUp(name, surname, email, password) {}
 
-	login(email,password){
-	
-	}
-
-	
-	signUp(name,surname,email,password){
-
-		
-	}
-
-	
-
-    signupValidationData(email, name, surname, password) {       
-        if(this.#validateEmail(email)===true && this.#validateName(name) === true 
-        && this.#validateName(surname) === true && this.#validatePassword(password)===true){
-          return  new UserModel (email, name, surname,password); 
-        }else{
-            return "Verifica os dados introduzidos";
-        }
+  signupValidationData(email, name, surname, password) {
+    if (
+      this.#validateEmail(email) === true &&
+      this.#validateName(name) === true &&
+      this.#validateName(surname) === true &&
+      this.#validatePassword(password) === true
+    ) {
+      return new UserModel(email, name, surname, password);
+    } else {
+      return "Verifica os dados introduzidos";
     }
+  }
 
-    #validateEmail(email){
-        let regExp = /^\w+[\w.-]*@\b(?:gmail|outlook|hotmail)\.com$/;
-        if (regExp.test(email) == true) {
-            return true;
-        }
-        return false;
+  #validateEmail(email) {
+    let regExp = /^\w+[\w.-]*@\b(?:gmail|outlook|hotmail)\.com$/;
+    if (regExp.test(email) == true) {
+      return true;
     }
+    return false;
+  }
 
-    #validateName(name){
-        let regExp = /^[a-zA-Z]+(([',.-][a-zA-Z])[a-zA-Z]*)*$/;
-        if (regExp.test(name) == true) {
-            return true;
-        }
-	    return false;
+  #validateName(name) {
+    let regExp = /^[a-zA-Z]+(([',.-][a-zA-Z])[a-zA-Z]*)*$/;
+    if (regExp.test(name) == true) {
+      return true;
     }
+    return false;
+  }
 
-    #validatePassword(password){
-        let regExp = /^[a-zA-Z]+(([',.-][a-zA-Z])[a-zA-Z]*)*$/;
-        if (regExp.test(password) == true) {
-            return true;
-        }
-        return false;
+  #validatePassword(password) {
+    let regExp = /^[a-zA-Z]+(([',.-][a-zA-Z])[a-zA-Z]*)*$/;
+    if (regExp.test(password) == true) {
+      return true;
     }
+    return false;
+  }
 }
